8774dc11fae08958162645a0cce223a33cda28b9
svper <verhulstrobin@gmail.com>
2015-01-28 03:27:00 +0100

    it's working

--- Backend/server.php
+++ Backend/server.php
@@ -1,14 +1,16 @@
 <?php
+$data = file_get_contents("php://input");
 
-function escape ($input)
+
+function escape($input)
 {
-    if (! get_magic_quotes_gpc()) {
+    if (!get_magic_quotes_gpc()) {
         $input = mysql_real_escape_string($input);
     }
     return $input;
 }
 
-function toNull ($input, $quote)
+function toNull($input, $quote)
 {
     if ($input == "") {
         return "NULL";
@@ -21,68 +23,171 @@ function toNull ($input, $quote)
     }
 }
 
+class Item
+{
+    public $Id;
+    public $ShopId;
+    public $Description;
+    public $Quantity;
+    public $ShopName;
+
+    // Assigning the values
+    public function __construct($Id, $ShopId, $Description, $Quantity, $ShopName)
+    {
+        $this->Id = $Id;
+        $this->ShopId = $ShopId;
+        $this->Description = $Description;
+        $this->Quantity = $Quantity;
+        $this->ShopName = $ShopName;
+    }
+
+}
+
+class Shop
+{
+    public $Id;
+    public $Description;
+
+    // Assigning the values
+    public function __construct($Id, $Description)
+    {
+        $this->Id = $Id;
+        $this->Description = $Description;
+    }
+
+}
 
 // Public
 $servername = "localhost";
-$username = "deb38057_shop";
-$password = "rvshop";
+$username = "root";
+$password = "";
 $dbname = "deb38057_shop";
 
 
+function getShops()
+{
+    $servername = "localhost";
+    $username = "deb38057_shop";
+    $password = "rvshop";
+    $dbname = "deb38057_shop";
 
-
-
-function getShops(){
-	$servername = "localhost";
-$username = "deb38057_shop";
-$password = "rvshop";
-$dbname = "deb38057_shop";
-	// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM `Shop`";
+    $result = $conn->query($sql);
+
+    $jsonData = array();
+    while ($array = $result->fetch_row()) {
+        $obj = new Shop($array[0], $array[1]);
+        $jsonData[] = $obj;
+    }
+    return json_encode($jsonData);
+
+
 }
 
-	$sql = "SELECT * FROM `Shop`";
-$result = $conn->query($sql);
+function done()
+{
+    $_GET["id"];
 
-$jsonData = array();
-while ($array = $result->fetch_row()) {
-    $jsonData[] = $array;
-}
-echo json_encode($jsonData);
+    $servername = "localhost";
+    $username = "deb38057_shop";
+    $password = "rvshop";
+    $dbname = "deb38057_shop";
 
-}
-
-
-function getItemLists(){
-	$servername = "localhost";
-$username = "deb38057_shop";
-$password = "rvshop";
-$dbname = "deb38057_shop";
-	
-	// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE `ListItem` SET Done=1 WHERE Id=" . $_GET["id"];
+
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
 }
-	$sql = "SELECT l.*,s.Name FROM `ListItem` l , `Shop` s WHERE l.ShopId = s.Id";
-$result = $conn->query($sql);
 
-$jsonData = array();
-while ($array = $result->fetch_row()) {
-    $jsonData[] = $array;
+function add($item)
+{
+
+    if (!isset($item->ShopId)) $item->ShopId = 1;
+
+        $servername = "localhost";
+    $username = "deb38057_shop";
+    $password = "rvshop";
+    $dbname = "deb38057_shop";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    if (isset($item->Quantity))
+        $sql = "INSERT INTO listitem (ShopId,Description, Quantity) VALUES (" . $item->ShopId . "," . toNull($item->Description,true) . "," . toNull($item->Quantity,true) . ")";
+    else
+        $sql = "INSERT INTO listitem (ShopId,Description) VALUES (" . $item->ShopId . "," . toNull($item->Description,true) . ")";
+
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
-return json_encode($jsonData);
+
+
+function getItemLists()
+{
+    $servername = "localhost";
+    $username = "deb38057_shop";
+    $password = "rvshop";
+    $dbname = "deb38057_shop";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT l.*,s.Name FROM `ListItem` l , `Shop` s WHERE l.ShopId = s.Id AND l.Done = 0";
+    $result = $conn->query($sql);
+
+    $jsonData = array();
+    while ($array = $result->fetch_row()) {
+        $obj = new Item($array[0], $array[1], $array[2], $array[3], $array[4]);
+        $jsonData[] = $obj;
+    }
+    return json_encode($jsonData);
 
 }
 
 
-
-echo getShops();
-echo getItemLists();
-
-
+$objData = json_decode($data);
+//echo getItemLists();
+switch ($_GET['method']) {
+    case "items":
+        echo getItemLists();
+        break;
+    case "shops":
+        echo getShops();
+        break;
+    case "done":
+        echo done();
+        break;
+    case "add":
+        //echo 'test';
+        //echo $data;
+        //print_r($objData->item);
+        echo add($objData->item);
+        break;
+}
 ?>
--- app/List/List.js
+++ app/List/List.js
@@ -9,6 +9,43 @@ angular.module('myApp.List', ['ngRoute'])
   });
 }])
 
-.controller('ListCtrl', [function() {
+.controller('ListCtrl', ['$scope','$http',function($scope,$http) {
+        $scope.filters = { };
 
-}]);
+        $scope.getShops = function(){
+            $http.post('../Backend/server.php?method=shops', { }).
+                success(function(data, status) {
+                    $scope.shops = data;
+                });
+        };
+
+        $scope.getItems = function(){
+            $http.post('../Backend/server.php?method=items', { }).
+                success(function(data, status) {
+                    $scope.items = data;
+                });
+        };
+
+        $scope.save = function(item){
+            if(item && item.Description){
+                console.log(item);
+                $http.post('../Backend/server.php?method=add', { "item": item}).
+                    success(function(data, status) {
+                        $scope.getItems();
+                    });
+            }
+        };
+
+        $scope.delete = function(id){
+            console.log(id);
+            $http.post('../Backend/server.php?method=done&id=' +id, { }).
+                success(function(data, status) {
+                    $scope.getItems();
+                });
+        };
+
+
+        $scope.getItems();
+        $scope.getShops();
+
+    }]);
